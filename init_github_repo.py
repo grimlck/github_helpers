6a240e8a081fd723a648d4cf3873afa76d430687
Micha <michael@swarrrm.de>
2012-12-21 20:10:26 +0100

    Initial commit

---
+++ init_github_repo.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import urllib2
+import json
+import base64
+import sys
+
+user_setting = {"username": "",
+                "password": ""
+               }
+
+auth_settings = {"scopes":'["public_repo"]'}
+
+def request_token(username, password, scope="", note=""):
+    """request a token from GitHub to access your repository
+    scope variable can have following values:
+    public, public_repo, repo, repo:status, delete_repo, notifications, gist
+    see http://developer.github.com/v3/oauth/#scopes for explanations"""
+    
+    if username and password:
+        # GitHub expects username:password to be base64 encoded
+        base64string = base64.encodestring(str(username)+":"+str(password)).replace('\n','')
+
+        # build the Request object
+        request = urllib2.Request("https://api.github.com/authorizations")
+        request.add_header("Authorization", "Basic %s" % base64string)
+
+        try:
+            result = urllib2.urlopen(request, '{"note":'+str(note)+'", "scopes":'+str(scope)+'}')
+            
+            # as the response comes in JSON format, we need to deserialize it to a Python object
+            result = json.loads('\n'.join(result.readlines()))
+            
+            if result['token'] and type(result) == dict:
+                return result['token']
+        
+        except urllib2.HTTPError as e:
+            sys.exit()
+
+    else:
+        sys.exit("Cannot request token, username or password not specified")
+
+
+    
+
