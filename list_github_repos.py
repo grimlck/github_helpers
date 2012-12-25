02f3d170f9bd4e387e40447873a550a7f8976440
Micha <michael@swarrrm.de>
2012-12-25 13:14:07 +0100

    pep8-ified

--- list_github_repos.py
+++ list_github_repos.py
@@ -6,38 +6,49 @@ import json
 import sys
 import argparse
 
+
 def get_repositories(github_user):
     """get the GitHub repositories for the given GitHub account.
-    The return value is a list of dictionaries which contain the repository data.
-    Available keys can be found here:
+    The return value is a list of dictionaries which contain the
+    repository data. Available keys can be found here:
     http://developer.github.com/v3/repos/"""
-    
+
     if not github_user:
         return [1, {"message": "GitHub username missing"}]
     else:
 
         # build Request object
-        request = urllib2.Request("https://api.github.com/users/"+str(github_user)+"/repos")
+        request = urllib2.Request("https://api.github.com/users/"
+                                  + str(github_user) + "/repos")
         request.get_method = lambda: 'GET'
         try:
-            # try to send the request to the GitHub API and create Python dictionary from JSON response
-
+            '''try to send the request to the GitHub API and
+            create Python dictionary from JSON response'''
             repositories = urllib2.urlopen(request)
             repositories = json.loads("\n".join(repositories.readlines()))
-            
+
             return [0, repositories]
-        
+
         except urllib2.HTTPError as e:
 
-            # return HTTP error and the message from the API  
-            return [1, {"message": str(e)+": "+json.loads('\n'.join(e.readlines()))['message']}]
+            # return HTTP error and the message from the API
+            return [1, {"message": str(e) + ": "
+                        + json.loads('\n'.join(e.readlines()))['message']}]
+
 
 def main():
 
-    # build argument parser, takes two arguments: GitHub username and number of repositories to show 
-    ap = argparse.ArgumentParser(description="List GitHub repositories by user")
+    #build argument parser, takes two arguments:
+    #GitHub username and number of repositories to show
+    ap = argparse.ArgumentParser(
+        description="List GitHub repositories by user"
+    )
     ap.add_argument("username", type=str, help="GitHub username")
-    ap.add_argument("-n", dest="show_only", metavar="number", type=int, help="Show only first n repositories", default=-1)
+    ap.add_argument("-n", dest="show_only",
+                    metavar="number",
+                    type=int,
+                    help="Show only first n repositories",
+                    default=-1)
     args = ap.parse_args()
 
     repositories = get_repositories(args.username)
@@ -61,9 +72,7 @@ def main():
             print "\n"
     else:
         print repositories[1]['message']
-        
 
 
 if __name__ == "__main__":
     main()
-
